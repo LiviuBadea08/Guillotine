74c5a58177f69564fab5e9b16efe61d3ab9a5318
LiviuBadea08 <liviubadea08@gmail.com>
2022-03-04 09:44:57 +0100

    audio added

--- js/index.js
+++ js/index.js
@@ -1,104 +1,113 @@
-let codersNames = ["Jessica Mejia", "Liviu Badea", "Abde Belkhialat", "Adrián Pelayo", "Damaris Teoc", "Ales", "Daniel Calvo", "Àlex Vidal", "Scarlet Rosa", "Albert Martinez", "Ingrid Alvarez", "Uri Codina", "Sergi Aparicio", "Guillermo", "Miguel Angel"];
+let codersNames = [
+  "Jessica Mejia",
+  "Liviu Badea",
+  "Abde Belkhialat",
+  "Adrián Pelayo",
+  "Damaris Teoc",
+  "Ales",
+  "Daniel Calvo",
+  "Àlex Vidal",
+  "Scarlet Rosa",
+  "Albert Martinez",
+  "Ingrid Alvarez",
+  "Uri Codina",
+  "Sergi Aparicio",
+  "Guillermo",
+  "Miguel Angel",
+];
 let deadCodersNames = [];
-let aliveMenuButton = document.getElementById('boton-vivos');
-let deadMenuButton = document.getElementById('boton-muertos');
-let myAliveMenu = document.getElementById('menu-vivos');
-let myDeadMenu = document.getElementById('menu-muertos')
-let alivePeopleMenu = document.getElementById('vivos');
-let deadPeopleMenu = document.getElementById('muertos');
+let aliveMenuButton = document.getElementById("boton-vivos");
+let deadMenuButton = document.getElementById("boton-muertos");
+let myAliveMenu = document.getElementById("menu-vivos");
+let myDeadMenu = document.getElementById("menu-muertos");
+let alivePeopleMenu = document.getElementById("vivos");
+let deadPeopleMenu = document.getElementById("muertos");
 
+const deadPerson = document.getElementById("person");
+const startButton = document.getElementById("start");
+const resetButton = document.getElementById("reset");
 
-const deadPerson = document.getElementById('person');
-const startButton = document.getElementById('start');
-const resetButton = document.getElementById('reset');
+function randomName(max) {
+  resultRandomName = Math.floor(Math.random() * max);
 
-function randomName(max){
-
-    resultRandomName = Math.floor(Math.random()*max);
-
-    return resultRandomName;
-
+  return resultRandomName;
 }
 
-function killCoder(coderList, randomNumber, deadList){
-    let deadCoder = "";
-    
-    for (let index = 0; index < coderList.length; index++) {
-        if (index == randomNumber) {
-            deadCoder = coderList.splice(index, 1).toString();
-            deadList.unshift(deadCoder);
-        }
+function killCoder(coderList, randomNumber, deadList) {
+  let deadCoder = "";
+
+  for (let index = 0; index < coderList.length; index++) {
+    if (index == randomNumber) {
+      deadCoder = coderList.splice(index, 1).toString();
+      deadList.unshift(deadCoder);
     }
+  }
 
-    return deadCoder;
-
+  return deadCoder;
 }
 
-function start(coderList, deadList){
+function start(coderList, deadList) {
+  if (coderList.length == 0) {
+    location.href = "./endgame.html";
+  }
 
-    if(coderList.length == 0) {
-        location.href = "./endgame.html";
-    }
-    
-    let max = coderList.length;
-    let randomNumber = randomName(max);
-    let deadCoder = killCoder(coderList, randomNumber, deadList);
+  let max = coderList.length;
+  let randomNumber = randomName(max);
+  let deadCoder = killCoder(coderList, randomNumber, deadList);
 
-    deadPerson.innerHTML = deadCoder;
+  deadPerson.innerHTML = deadCoder;
+}
 
-} 
+function reset(coderList, deadList) {
+  let deadCoders = deadList.splice(0, deadList.length);
 
-function reset(coderList,deadList){
+  codersNames = coderList.concat(deadCoders);
 
-    let deadCoders = deadList.splice(0, deadList.length);
-
-    codersNames = coderList.concat(deadCoders);
-
-    deadPerson.innerHTML = "";
-    
+  deadPerson.innerHTML = "";
 }
 
 function makeTextFromArray(array) {
-    let text = '';
-    for (let index = 0; index < array.length; index++) {
-        text += array[index];
-        text += "\n";
-    }
-    return text;
+  let text = "";
+  for (let index = 0; index < array.length; index++) {
+    text += array[index];
+    text += "\n";
+  }
+  return text;
 }
 
 function toggleMenu(firstMenu, secondMenu) {
-
-    firstMenu.classList.toggle('show');
-    secondMenu.classList.remove('show');
-
+  firstMenu.classList.toggle("show");
+  secondMenu.classList.remove("show");
 }
 
 function showAliveMenu() {
+  toggleMenu(myAliveMenu, myDeadMenu);
 
-    toggleMenu(myAliveMenu, myDeadMenu);
+  let coderList = makeTextFromArray(codersNames);
 
-    let coderList = makeTextFromArray(codersNames);
-
-    alivePeopleMenu.innerText = coderList;
+  alivePeopleMenu.innerText = coderList;
 }
 
 function showDeadMenu() {
+  toggleMenu(myDeadMenu, myAliveMenu);
 
-    toggleMenu(myDeadMenu, myAliveMenu);
+  let coderList = makeTextFromArray(deadCodersNames);
 
-    let coderList = makeTextFromArray(deadCodersNames);
-
-    deadPeopleMenu.innerText = coderList;
+  deadPeopleMenu.innerText = coderList;
 }
 
-aliveMenuButton.addEventListener('click', showAliveMenu);
-deadMenuButton.addEventListener('click', showDeadMenu);
+aliveMenuButton.addEventListener("click", showAliveMenu);
+deadMenuButton.addEventListener("click", showDeadMenu);
 
-startButton.addEventListener('click', () => {
-    start(codersNames, deadCodersNames);
+startButton.addEventListener("click", () => {
+  start(codersNames, deadCodersNames);
 });
 
-resetButton.addEventListener('click', () => {
-    reset(codersNames, deadCodersNames);
+resetButton.addEventListener("click", () => {
+  reset(codersNames, deadCodersNames);
 });
+
+function sound() {
+  let snd = new Audio("../sound/guillo.mp4");
+  snd.play();
+}
